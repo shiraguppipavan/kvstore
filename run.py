6a4c0a1284c949724d2ee57281fa9f369105433e
Pavan Shiraguppi <shiraguppipavan@gmail.com>
2023-07-21 00:45:04 +0530

    [UPDATE] 20230721 Updated the prometheus http server, metrics info and added port configuration

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 from waitress import serve
 from dotenv import load_dotenv
-from app.kvstore_app import app
+from app.kvstore_app import app, start_prometheus_server
 
 load_dotenv()
 
@@ -9,4 +9,5 @@ flask_host = os.getenv("HOST")
 flask_port = os.getenv("PORT")
 
 if __name__ == "__main__":
+    start_prometheus_server()
     serve(app=app, host=flask_host, port=flask_port)
